ac69c9a5be4eccac70579f999e2777f19e71dcd7
Nick <Nick@DESKTOP-82IOVQF>
2025-03-06 08:19:29 -0600

    Add project files.

---
+++ week7prac/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7prac
+{
+    internal class Program
+    {
+        public static int CountWords(string fileName) 
+        {
+            string content = File.ReadAllText(fileName);
+            string[] strings;
+            strings = content.Split(' ');
+            int count = strings.Length;
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string words = "Hello world i am world";
+            string fileName = "filename.txt";
+
+            File.WriteAllText(fileName, words);
+            Console.WriteLine("There are " + CountWords(fileName) + "words in the string");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week7prac/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("week7prac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("week7prac")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("664209b9-3fb7-4f7a-ad9a-b10f78f6522b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
